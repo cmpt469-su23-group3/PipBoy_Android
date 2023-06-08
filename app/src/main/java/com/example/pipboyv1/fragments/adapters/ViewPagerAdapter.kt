823926586085a1bcb8c9bd667dd0a15911f06beb
cbarkr <cbarkr@gmail.com>
2023-06-07 17:44:50 -0700

    Add viewpager adapter

---
+++ app/src/main/java/com/example/pipboyv1/fragments/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.pipboyv1.fragments.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+    private val fragmentList = ArrayList<Fragment>()
+    private val fragmentTitleList = ArrayList<String>()
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        fragmentTitleList.add(title)
+    }
+}
