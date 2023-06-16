9bcedbfe0dacba9e681ef8f687167fe035c3d372
cbarkr <cbarkr@gmail.com>
2023-06-16 13:18:29 -0700

    Make strings non-nullable

--- app/src/main/java/com/example/pipboyv1/classes/SelectionItem.kt
+++ app/src/main/java/com/example/pipboyv1/classes/SelectionItem.kt
@@ -1,9 +1,9 @@
 package com.example.pipboyv1.classes
 
 open class SelectionItem(
-    val textLeft: String? = "",
-    val textCenter: String? = "",
-    val textRight: String? = "",
+    val textLeft: String = "",
+    val textCenter: String = "",
+    val textRight: String = "",
     var selected: Boolean = false,
     var data: SelectionItemData = SelectionItemData()
 )
--- app/src/main/java/com/example/pipboyv1/classes/SelectionItemData.kt
+++ app/src/main/java/com/example/pipboyv1/classes/SelectionItemData.kt
@@ -1,7 +1,7 @@
 package com.example.pipboyv1.classes
 
 open class SelectionItemData(
-    val description: String? = "",
+    val description: String = "",
     val image: Int? = 0,
     val attributes: Map<String, String> = mapOf()
 )
