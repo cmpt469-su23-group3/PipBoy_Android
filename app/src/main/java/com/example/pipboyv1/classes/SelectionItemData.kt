b483863b1ae5de603c92e99e6e9b9f30413f82c4
cbarkr <cbarkr@gmail.com>
2023-06-15 17:11:46 -0700

    Add attributes map to `SelectionItemData`

--- app/src/main/java/com/example/pipboyv1/classes/SelectionItemData.kt
+++ app/src/main/java/com/example/pipboyv1/classes/SelectionItemData.kt
@@ -3,4 +3,5 @@ package com.example.pipboyv1.classes
 open class SelectionItemData(
     val description: String? = "",
     val image: Int? = 0,
+    val attributes: Map<String, String> = mapOf()
 )
