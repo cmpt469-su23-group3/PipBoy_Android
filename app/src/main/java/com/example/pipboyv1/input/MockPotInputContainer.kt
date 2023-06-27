f219f1636767594ae83460692322150d79686b38
jwa359 <jwa359@sfu.ca>
2023-06-27 10:26:12 -0700

    Resolved requested changes as per merge request

--- app/src/main/java/com/example/pipboyv1/input/MockPotInputContainer.kt
+++ app/src/main/java/com/example/pipboyv1/input/MockPotInputContainer.kt
@@ -4,11 +4,8 @@ import kotlin.Exception
 
 
 class MockPotInputContainer : IPotInputContainer {
-    
+    private val mockPotValues: FloatArray = FloatArray(3) { 0F }
     private val listeners: MutableSet<PotInputListener> = mutableSetOf()
-    companion object {
-        private var mockPotValues: FloatArray = FloatArray(3) { 0F }
-    }
 
     override fun addListener(listener: PotInputListener) {
         listeners += listener
@@ -20,13 +17,9 @@ class MockPotInputContainer : IPotInputContainer {
     
     // TODO implement your mocks for calling the PotInputListener functions
     fun setPotValue(potIndex: Int, percentageValue: Float) {
-        // only allow values 0, 1, 2
-        if (potIndex < 0 || potIndex > 2) {
-            throw Exception("Invalid potentiometer index.")
-        }
-        // only allow values between 0 and 100
-        if (percentageValue < 0F || percentageValue > 100F) {
-            throw Exception("Potentiometer value to be set must be between 0 and 100.")
+        checkPotIndex(potIndex)
+        if (percentageValue !in 0F..100F) {
+            throw Exception("Potentiometer value to be set must be between 0 and 100. Given value: $percentageValue")
         }
 
         mockPotValues[potIndex] = percentageValue
@@ -37,10 +30,9 @@ class MockPotInputContainer : IPotInputContainer {
 
     // Left means decreasing the value and right means increasing the value
     fun moveLeft(potIndex: Int, decrementValue: Float) {
-        if (potIndex < 0 || potIndex > 2) {
-            throw Exception("Invalid potentiometer index.")
-        } else if (decrementValue < 0F) {
-            throw Exception("Potentiometer value to decrement must not be negative.")
+        checkPotIndex(potIndex)
+        if (decrementValue < 0F) {
+            throw Exception("Potentiometer value to increment must not be negative. Given value is: $decrementValue")
         }
         if (mockPotValues[potIndex] - decrementValue < 0F) {
             // could show a debug pop-up window here
@@ -53,10 +45,9 @@ class MockPotInputContainer : IPotInputContainer {
     }
 
     fun moveRight(potIndex: Int, incrementValue: Float) {
-        if (potIndex < 0 || potIndex > 2) {
-            throw Exception("Invalid potentiometer index.")
-        } else if (incrementValue < 0F) {
-            throw Exception("Potentiometer value to increment must not be negative.")
+        checkPotIndex(potIndex)
+        if (incrementValue < 0F) {
+            throw Exception("Potentiometer value to increment must not be negative. Given value is: $incrementValue")
         }
         if (mockPotValues[potIndex] + incrementValue > 100F) {
             // could show a debug pop-up window here
@@ -65,6 +56,12 @@ class MockPotInputContainer : IPotInputContainer {
 
         for (listener in listeners) {
             listener.onMoveRight(potIndex, mockPotValues[potIndex])
+        }
+    }
+
+    private final fun checkPotIndex(potIndex: Int) {
+        if (potIndex !in 0..2) {
+            throw Exception("Invalid potentiometer index. Given index is: $potIndex")
         }
     }
 }
