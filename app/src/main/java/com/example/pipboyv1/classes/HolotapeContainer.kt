fcdaf838522b14917004772ca3c955eabe728554
cbarkr <cbarkr@gmail.com>
2023-07-13 15:58:00 -0700

    Add container for holotapes and sully's journal tape

---
+++ app/src/main/java/com/example/pipboyv1/classes/HolotapeContainer.kt
@@ -0,0 +1,28 @@
+package com.example.pipboyv1.classes
+
+object HolotapeContainer {
+
+    val holotapes: List<Holotape> = listOf(
+        Holotape(0, mapOf(
+            "[Sully's Log 6/17]" to
+                    "This is a great spot. With the right crew I'd have no problem keeping this place locked down. Traders run close enough for easy pickins, but not so close that we end up skinned.\n" +
+                    "\n" +
+                    "The whole damn thing's filled with water, though! Figures. There's always a catch.\n" +
+                    "\n" +
+                    "Bigass drainage pump looks mostly intact. If I'm lucky it might still work. Gonna take a closer look tomorrow.\n",
+            "[Sully's Log 6/18]" to
+                    "Nope. Not gonna be that easy. Still - pump is in better shape than you'd think.\n" +
+                    "\n" +
+                    "I'd like to head back and grab some tools, but it's pretty late. I'll tinker with it some before I bed down tonight, then head out in the morning.\n",
+            "[Sully's Log 6/20]" to
+                    "Shouldn't be too hard now. Glad I grabbed that sledge, even if it was a bitch to carry all this way. Thing was rusted tight. Few good whacks with the sledge cleared that problem right up.\n" +
+                    "\n" +
+                    "Thing made the damndest sound each time, though. You'd swear something was moving around in there.\n",
+            "[Sully's Log 6/22]" to
+                    "Was able to make most of the connections. Patched up all the pipe leaks I could find, besides the ones in the water.\n" +
+                    "\n" +
+                    "All the noises I've been hearing, I'm not setting foot in there. I'll probably head back and drag one of the new guys over here and make him do it.\n",
+        )),
+    )
+
+}
